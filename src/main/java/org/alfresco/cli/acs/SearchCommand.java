3368214a213b97d332c9931a5cbd24051b96820b
Angel Borroy <angel.borroy@alfresco.com>
2021-03-17 12:49:24 +0100

    Search command simplified and formatting result.

--- src/main/java/org/alfresco/cli/acs/SearchCommand.java
+++ src/main/java/org/alfresco/cli/acs/SearchCommand.java
@@ -1,99 +1,119 @@
 package org.alfresco.cli.acs;
 
-import java.util.List;
-import java.util.concurrent.Callable;
+import org.alfresco.cli.format.FormatProvider;
+import org.alfresco.cli.format.FormatProviderRegistry;
 import org.alfresco.search.handler.SearchApi;
-import org.alfresco.search.model.RequestPagination;
-import org.alfresco.search.model.RequestQuery;
-import org.alfresco.search.model.ResultSetRowEntry;
-import org.alfresco.search.model.SearchRequest;
+import org.alfresco.search.model.*;
 import org.alfresco.search.sql.handler.SqlApi;
+import org.alfresco.search.sql.model.SQLResultSetPagingList;
 import org.alfresco.search.sql.model.SQLResultSetRowEntry;
 import org.alfresco.search.sql.model.SQLSearchRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import picocli.CommandLine.ArgGroup;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Mixin;
 import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.util.List;
 
 @Component
-@Command(name = "search", subcommands =
-        {SearchCommand.CmisQueryCommand.class, SearchCommand.FtsQueryCommand.class, SearchCommand.SqlQueryCommand.class})
+@Command(name = "search", description = "Search commands")
 public class SearchCommand {
 
-    static abstract class AbstractSearchCommand implements Callable<Integer> {
+    @Mixin
+    FormatProviderRegistry formatProvider;
 
-        @Autowired
-        SearchApi searchApi;
+    @Autowired
+    SearchApi searchApi;
 
-        @Option(names = {"-q", "--query"}, required = true, description = "Query (examples >> \"select * from cmis:folder\", \"foo\")")
-        String query;
+    @Autowired
+    private SqlApi sqlApi;
 
-        @ArgGroup(exclusive = false, multiplicity = "0..1", heading = "Paging Options")
-        PagingOptions pagingOptions;
+    @Command(description = "Execute cmis query.")
+    public Integer cmis(@Parameters(description = "Query (example >> \"select * from cmis:folder\"") String query,
+                        @Option(names = {"-pmi", "--paging-max-items"}, defaultValue = "100", description = "Max Items per Page") Integer pagingMaxItems,
+                        @Option(names = {"-psc", "--paging-skip-count"}, defaultValue = "0", description = "Skip Count initial results") Integer pagingSkipCount) {
+        ResultSetPagingList result =
+                searchApi.search(new SearchRequest()
+                        .query(new RequestQuery()
+                                .language(RequestQuery.LanguageEnum.CMIS)
+                                .query(query))
+                        .paging(new RequestPagination()
+                                .maxItems(pagingMaxItems)
+                                .skipCount(pagingSkipCount))).getBody().getList();
+        formatProvider.print(result);
+        return 0;
+    }
 
-        SearchRequest getSearchRequest(RequestQuery.LanguageEnum language) {
-            SearchRequest searchRequest = new SearchRequest()
-                    .query(new RequestQuery()
-                            .language(language)
-                            .query(query));
-            if (pagingOptions != null) {
-                searchRequest.paging(new RequestPagination()
-                        .maxItems(pagingOptions.pagingMaxItems)
-                        .skipCount(pagingOptions.pagingSkipCount));
-            }
-            return searchRequest;
+    @Command(description = "Execute fts query.")
+    public Integer fts(@Parameters(description = "Query (example >> \"cm:title:foo\")") String query,
+                       @Option(names = {"-pmi", "--paging-max-items"}, defaultValue = "100", description = "Max Items per Page") Integer pagingMaxItems,
+                       @Option(names = {"-psc", "--paging-skip-count"}, defaultValue = "0", description = "Skip Count initial results") Integer pagingSkipCount) {
+        ResultSetPagingList result =
+                searchApi.search(new SearchRequest()
+                        .query(new RequestQuery()
+                                .language(RequestQuery.LanguageEnum.AFTS)
+                                .query(query))
+                        .paging(new RequestPagination()
+                                .maxItems(pagingMaxItems)
+                                .skipCount(pagingSkipCount))).getBody().getList();
+        formatProvider.print(result);
+        return 0;
+    }
+
+    @Command(description = "Execute sql query.")
+    public Integer sql(@Parameters(description = "SQL Query (example >> \"select * from alfresco\")") String query) {
+        SQLResultSetPagingList result = sqlApi.search(new SQLSearchRequest().stmt(query)).getBody().getList();
+        formatProvider.print(result);
+        return 0;
+    }
+
+    @Component
+    static class ResultSetPagingListProvider implements FormatProvider {
+
+        @Override
+        public void print(Object item) {
+            final ResultSetPagingList resultSetList = (ResultSetPagingList) item;
+            List<ResultSetRowEntry> entries = resultSetList.getEntries();
+            System.out.println("----------------------------------------------------------------------------------------------------------------------");
+            System.out.printf("%-40s %-40s %-20s", "ID", "NAME", "TYPE");
+            System.out.println();
+            System.out.println("----------------------------------------------------------------------------------------------------------------------");
+            entries.stream().map(entry -> entry.getEntry()).forEach(entry -> {
+                System.out.printf("%-40s %-40s %-20s", entry.getId(), entry.getName(), entry.getNodeType());
+                System.out.println();
+            });
+            System.out.println("----------------------------------------------------------------------------------------------------------------------");
         }
 
-        static class PagingOptions {
-            @Option(names = {"-pmi", "--paging-max-items"}, description = "Max Items per Page", required = true)
-            private Integer pagingMaxItems;
-            @Option(names = {"-psc", "--paging-skip-count"}, description = "Skip Count initial results", required = true)
-            private Integer pagingSkipCount;
+        @Override
+        public boolean isApplicable(Class<?> itemClass, String format) {
+            return DEFAULT.equals(format) && ResultSetPagingList.class == itemClass;
         }
     }
 
     @Component
-    @Command(name = "cmis")
-    static class CmisQueryCommand extends AbstractSearchCommand {
+    static class SQLResultSetPagingListProvider implements FormatProvider {
 
         @Override
-        public Integer call() {
-            List<ResultSetRowEntry> result =
-                    searchApi.search(getSearchRequest(RequestQuery.LanguageEnum.CMIS)).getBody().getList().getEntries();
-            System.out.println(result);
-            return 0;
+        public void print(Object item) {
+            final SQLResultSetPagingList resultSetList = (SQLResultSetPagingList) item;
+            List<SQLResultSetRowEntry> entries = resultSetList.getEntries();
+            System.out.println("----------------------------------------------------------------------------------------------------------------------");
+            System.out.printf("%-40s %-80s", "FIELD", "VALUE");
+            System.out.println();
+            System.out.println("----------------------------------------------------------------------------------------------------------------------");
+            entries.stream().forEach(entry -> {
+                System.out.printf("%-40s %-80s", entry.getLabel(), entry.getValue());
+                System.out.println();
+            });
+            System.out.println("----------------------------------------------------------------------------------------------------------------------");
         }
-    }
-
-    @Component
-    @Command(name = "fts")
-    static class FtsQueryCommand extends AbstractSearchCommand {
 
         @Override
-        public Integer call() throws Exception {
-            List<ResultSetRowEntry> result =
-                    searchApi.search(getSearchRequest(RequestQuery.LanguageEnum.AFTS)).getBody().getList().getEntries();
-            System.out.println(result);
-            return 0;
-        }
-    }
-
-    @Component
-    @Command(name = "sql")
-    static class SqlQueryCommand implements Callable<Integer> {
-
-        @Autowired
-        private SqlApi sqlApi;
-
-        @Option(names = {"-q", "--query"}, required = true, description = "SQL Query (example >> \"select * from alfresco\")")
-        private String query;
-
-        @Override
-        public Integer call() throws Exception {
-            List<SQLResultSetRowEntry> result = sqlApi.search(new SQLSearchRequest().stmt(query)).getBody().getList().getEntries();
-            System.out.println(result);
-            return 0;
+        public boolean isApplicable(Class<?> itemClass, String format) {
+            return DEFAULT.equals(format) && SQLResultSetPagingList.class == itemClass;
         }
     }
 
